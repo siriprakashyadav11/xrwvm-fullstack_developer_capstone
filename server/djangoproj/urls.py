efce250251065db677251354f9c1bbadbface37e
siriprakashyadav11 <siriprakashyadav11@gmail.com>
2024-05-29 08:52:57 -0400

    hola

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,10 +21,14 @@ from django.conf import settings
 
 urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html")),
-        path('login/', TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
