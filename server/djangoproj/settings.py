796120c9bccd592026230fddfcd5046730dbe71f
siriprakashyadav11 <111074706+siriprakashyadav11@users.noreply.github.com>
2024-06-16 19:46:08 +0530

    committed

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,15 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','https://siriprakashy-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://siriprakashy-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://indhumuppall-8000.theiadockernext-1-labs-prod-theiak8s-4-'
+    'tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'https://indhumuppall-8000.theiadockernext-1-labs-prod-theiak8s-4-'
+    'tor01.proxy.cognitiveclass.ai']
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -62,12 +69,10 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            
             os.path.join(BASE_DIR, 'frontend/static'),
             os.path.join(BASE_DIR, 'frontend/build'),
             os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
-        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -96,7 +101,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -145,4 +151,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
